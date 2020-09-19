a2a922ec8310236a730eb76fd8589580dfaafafd
KoTubA <szymon.kotarba988@gmail.com>
2020-09-19 21:30:56 +0200

    Update files in project

--- JavaScript/Asynchronous JavaScript/Asynchronus JavaScript Project/main.js
+++ JavaScript/Asynchronous JavaScript/Asynchronus JavaScript Project/main.js
@@ -1,11 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
     let button = document.querySelector('.load-data'),
-        container = document.querySelector('.status-cnt');
+        container = document.querySelector('.status-cnt'),
+        i = 0;
 
     button.addEventListener('click', () => {
+        //Reset iteration
+        i = 0;
+        container.innerHTML = "";
+
+        //Callback Hell
+        getData("https://jsonplaceholder.typicode.com/users", "Loading users data", (status, data) => {
+            if (status) {
+                console.log(data);
+
+                getData("https://jsonplaceholder.typicode.com/posts", "Loading posts data", (status, data) => {
+                    if (status) {
+                        console.log(data);
+
+                        getData("https://jsonplaceholder.typicode.com/comments", "Loading comments data", (status, data) => {
+                            if (status) {
+                                console.log(data);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    });
+
+    function getData(url, text, callback) {
+        i++;
         let request = new XMLHttpRequest();
         let fragment = document.createDocumentFragment();
-        container.innerHTML = "";
 
         request.addEventListener('readystatechange', () => {
             if (request.readyState === 1) {
@@ -13,13 +39,13 @@ document.addEventListener("DOMContentLoaded", () => {
                     status_icon = document.createElement('img'),
                     status = document.createElement('div');
 
-                status_wrapper.classList.add('status-wrapper', 'status-wrapper-1');
+                status_wrapper.classList.add('status-wrapper', 'status-wrapper-' + i);
 
-                status_icon.classList.add('status-icon', 'status-icon-1');
+                status_icon.classList.add('status-icon', 'status-icon-' + i);
                 status_icon.src = "img/iconfinder_Clock_381590.png";
 
-                status.innerText = "Loading user data";
-                status.classList.add('status-1');
+                status.innerText = text;
+                status.classList.add('status-' + i);
 
                 status_wrapper.appendChild(status_icon);
                 status_wrapper.appendChild(status);
@@ -28,16 +54,16 @@ document.addEventListener("DOMContentLoaded", () => {
                 container.appendChild(fragment);
             }
             else if (request.readyState === 4 && request.status === 200) {
-                document.querySelector('.status-icon-1').src = "img/iconfinder_Complete_Symbol_381607.png";
-                console.log(request.response);
+                document.querySelector('.status-icon-' + i).src = "img/iconfinder_Complete_Symbol_381607.png";
+                callback(true, request.response);
             }
             else if (request.readyState === 4) {
-                document.querySelector('.status-icon-1').src = "img/iconfinder_Incorrect_Symbol_381610.png";
+                document.querySelector('.status-icon-' + i).src = "img/iconfinder_Incorrect_Symbol_381610.png";
+                callback(false, undefined);
             }
         });
 
-        request.open("GET", 'https://jsonplaceholder.typicode.com/users/1');
+        request.open("GET", url);
         request.send();
-
-    });
+    }
 });
