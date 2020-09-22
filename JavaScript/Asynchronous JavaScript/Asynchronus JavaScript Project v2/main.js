79079206096652a6b1a017f41a58d8a0439a0bbb
KoTubA <szymon.kotarba988@gmail.com>
2020-09-23 01:58:02 +0200

    Update form error handling

--- JavaScript/Asynchronous JavaScript/Asynchronus JavaScript Project v2/main.js
+++ JavaScript/Asynchronous JavaScript/Asynchronus JavaScript Project v2/main.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     let slider_previous_button = document.querySelector('.weather-data-daily-previous-button'),
         slider_next_button = document.querySelector('.weather-data-daily-next-button'),
         weather_data_daily_slider = document.querySelector('.weather-data-daily-slider'),
-        search_from = document.querySelector("#search-form");
+        search_from = document.querySelector("#search-form"),
+        weather_search = document.querySelector('.weather-search'),
+        weather_cnt = document.querySelector('.weather-cnt'),
+        form_input = document.querySelector('.form-input');
 
     slider_previous_button.addEventListener('click', () => { move(false) });
     slider_next_button.addEventListener('click', () => { move(true) });
@@ -21,6 +24,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
     search_from.addEventListener('submit', (e) => {
         e.preventDefault();
+
+        try {
+            if (form_input.value !== "") {
+                //Fetch API ... then:
+
+                weather_search.style.visibility = "hidden";
+                weather_cnt.style.visibility = "visible";
+            }
+            else {
+                throw new Error('Field cannot be empty');
+            }
+        }
+        catch (error) {
+            weather_search.classList.add('error');
+            document.querySelector('.error-message').innerText = error.message;
+        }
+
+    });
+
+    form_input.addEventListener('focus', () => {
+        weather_search.classList.remove('error');
     });
 
 });
