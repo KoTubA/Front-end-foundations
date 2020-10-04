3239713f584452d61317d6fba73eb7972f08a618
KoTubA <szymon.kotarba988@gmail.com>
2020-10-04 10:21:01 +0200

    Add simple npm project

--- NPM/index.js
+++ NPM/index.js
@@ -1,3 +1,8 @@
-$('h2').click(function () {
-    $('.overlay').css('transform', 'scale(2)');
-})
+const _ = require('lodash');
+
+const numbers = [2, 33, 11, 75];
+
+_.each(numbers, function (numbers) {
+    console.log(numbers);
+});
+
